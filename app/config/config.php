e41bf3ccc0c65d055b51295e1a1f52e4f7fbd795
LeviTas2003 <levitas2003@gmail.com>
2023-03-09 17:48:12 +0100

    OverzichtMenu  gemerged met de Develop Branch

--- app/config/config.php
+++ app/config/config.php
@@ -8,8 +8,8 @@ define('DB_NAME', 'rocambolesque');
 // Pad naar app map
 define('APPROOT', dirname(dirname(__FILE__)));
 
-// define('URLROOT', 'http://www.restaurant-rocambolesque.local');
-define('URLROOT', 'http://localhost:8080');
+define('URLROOT', 'http://www.restaurant-rocambolesque.local');
+// define('URLROOT', 'http://localhost:8080');
 
 define('SITENAME', 'Restaurant-Rocambolesque');
 // 
